9f764ba80cc0354ec444c1faac306fab5e3bd610
pivotmobileapp <jupiter@pivotmobileapp.com>
2020-04-28 12:00:24 -0400

    Initial commit

---
+++ platforms/browser/www/js/config.js
@@ -0,0 +1,6 @@
+var krms_config ={			
+	'ApiUrl':"https://paymeal.app/singlemerchant/api",			
+	'DialogDefaultTitle':"PayCart",			
+	'MerchantKeys':"1c630b2a586ef8ad95ecffa82571f3ff",
+	'debug': false
+};
---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+var krms_config ={			
+	'ApiUrl':"https://paymeal.app/singlemerchant/api",			
+	'DialogDefaultTitle':"PayCart",			
+	'MerchantKeys':"71701402510f8618c378712123484d99",
+	'debug': false
+};
