c6710e376b5b9b47a229a96222a083678f6f72e0
pivotmobileapp <jupiter@pivotmobileapp.com>
2020-05-04 18:54:47 -0400

    paycart

--- platforms/browser/www/js/config.js
+++ platforms/browser/www/js/config.js
@@ -1,6 +1,6 @@
 var krms_config ={			
-	'ApiUrl':"https://paymeal.app/singlemerchant/api",			
-	'DialogDefaultTitle':"PayCart",			
-	'MerchantKeys':"1c630b2a586ef8ad95ecffa82571f3ff",
+	'ApiUrl':"https://paycart.app/singlemerchant/api",			
+	'DialogDefaultTitle':"Geo's",			
+	'MerchantKeys':"71701402510f8618c378712123484d99",
 	'debug': false
 };
--- www/js/config.js
+++ www/js/config.js
@@ -1,5 +1,5 @@
 var krms_config ={			
-	'ApiUrl':"https://paymeal.app/singlemerchant/api",			
+	'ApiUrl':"https://paycart.app/singlemerchant/api",			
 	'DialogDefaultTitle':"Geo's",			
 	'MerchantKeys':"71701402510f8618c378712123484d99",
 	'debug': false
